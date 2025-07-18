3a77716e9d8a44a90c9aa8d7611d42b9ed5fba3b
vippium <93650504+vippium@users.noreply.github.com>
2025-07-18 18:26:34 +0530

    updated backend path

--- bookstore-frontend/src/services/axios.js
+++ bookstore-frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://bookstore-management-system-9im0.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
